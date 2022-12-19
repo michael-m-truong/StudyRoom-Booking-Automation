b6f48b58a113861f15dff4d9cb289ef6b9444dc0
Michael Truong <truong.minh.michael@gmail.com>
2022-12-18 16:58:26 -0800

    fixed import statement

--- Github_Driver.py
+++ Github_Driver.py
@@ -13,7 +13,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.utils import ChromeType
+from webdriver_manager.core.utils import ChromeType
 from selenium.webdriver.chrome.options import Options
 
 
