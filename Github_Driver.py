4e3e29aa5273ca9bbfe39e7b9bf7bbb3a6a8e27d
Michael Truong <73093864+michael-m-truong@users.noreply.github.com>
2023-04-13 13:35:52 -0700

    Update Github_Driver.py

--- Github_Driver.py
+++ Github_Driver.py
@@ -186,7 +186,7 @@ def getDatesInTable():
     try:
         alert = driver.switch_to.alert
         alert_text = alert.text
-        print("alert text: " + alert_text)
+        print("alert text: " + alert.get_attribute("innerHTML"))
         alert.dismiss()
         driver.switch_to.default_content() # switch back to the browser
     except NoAlertPresentException:
