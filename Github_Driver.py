9951b31e70d697d13dd8f6815f8f6d61f4ae4e23
Michael Truong <73093864+michael-m-truong@users.noreply.github.com>
2023-04-13 09:57:14 -0700

    import exception

--- Github_Driver.py
+++ Github_Driver.py
@@ -16,6 +16,7 @@ from selenium.webdriver.common.by import By
 
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoAlertPresentException
 
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.utils import ChromeType
