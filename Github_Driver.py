2000d12fe59d0efc5f31a7976dae8326433df5fd
Michael Truong <73093864+michael-m-truong@users.noreply.github.com>
2023-04-13 10:22:18 -0700

    switch back to browser

--- Github_Driver.py
+++ Github_Driver.py
@@ -183,6 +183,7 @@ def getDatesInTable():
     try:
         alert = driver.switch_to.alert
         alert.dismiss()
+        driver.switch_to.default_content() # switch back to the browser
     except NoAlertPresentException:
         pass
 
