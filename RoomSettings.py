c79479cfaefa1430ce1aca1d2fb4a39233ed3d32
Michael Truong <73093864+michael-m-truong@users.noreply.github.com>
2023-04-24 12:47:16 -0700

    Update RoomSettings.py

--- RoomSettings.py
+++ RoomSettings.py
@@ -15,7 +15,7 @@ def intializeRoomSettings():
     
     FAVORITE_TIMES_MW = ["10:00am", "10:30am"]
     FAVORITE_TIMES_TTH = ["2:30pm", "3:00pm"]
-    FAVORITE_ROOMS = ["4134", "3929", "4823", "4136"]
+    FAVORITE_ROOMS = ["4134", "3929", "3927", "4823", "4136"]
     FAVORITE_DATES = [days[0], days[1], days[2], days[3], days[4], days[5], days[6], days[7], days[8], days[9], days[10]]   #day[0] is tomorrow, day[1] 1 day after tom, etc.
     MINIMUM_TIME_ALLOTED = 4   # 4 30 min sessions, so 2 hrs
     return (FAVORITE_TIMES_MW, FAVORITE_TIMES_TTH, FAVORITE_ROOMS, FAVORITE_DATES, MINIMUM_TIME_ALLOTED)
