dbaa3cd7072da0fa6bf32c62e459aee79630eeae
Michael Truong <truong.minh.michael@gmail.com>
2023-08-02 21:31:47 -0700

    Make script production ready

--- Github_Driver.py
+++ Github_Driver.py
@@ -114,7 +114,7 @@ def getRoomFirst():
     gmt_now = utc_now.astimezone(gmt_tz)
 
     # Calculate the date one week in advance in GMT
-    gmt_one_week_in_advance = gmt_now + timedelta(days=6)  #testing
+    gmt_one_week_in_advance = gmt_now + timedelta(days=7)  #when testing make days < 7
 
     # Set the time to 00:00:00 for both dates
     gmt_today_date = gmt_now.replace(hour=0, minute=0, second=0, microsecond=0)
